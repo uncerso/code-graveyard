73ea8af38e4f710328462fd7c5d38f2ce218ed6f
uncerso <yayayayaya38@gmail.com>
2018-06-05 22:33:31 +0300

    beta

--- lastSteps/Tests/Network.fs
+++ lastSteps/Tests/Network.fs
@@ -1,106 +1,106 @@
-﻿open FsUnit
-open hw 
-open FsCheck
+﻿open hw
+open NUnit.Framework
+open FsUnit
 
 let nothingBesideZero (net : Network) =
     let mutable b = true
-    for x in net.getState() do
+    for x in net.GetState() do
         x |> should equal b
         b <- false
 
-let weak_virus ()= 
-    printfn "weak_virus : begin"
+[<Test>]
+let ``weakVirus`` ()= 
+    printfn "weakVirus : begin"
     let n = 10
     let net = Network((fun () -> 5ul))
-    printfn "weak_virus : point 1"
+    printfn "weakVirus : point 1"
     for i in 1..n do
-        net.addComputer(Computer(1ul))
-    printfn "weak_virus : point 2"
+        net.AddComputer(Computer(1ul))
+    printfn "weakVirus : point 2"
     for i in 1..n-1 do
-        net.addEdge 0 i
-    printfn "weak_virus : point 3"
-    net.addVirus 0
+        net.AddEdge 0 i
+    printfn "weakVirus : point 3"
+    net.AddVirus 0
     nothingBesideZero net
     for i in 0..10000 do
-        net.step()
+        net.Step()
         nothingBesideZero net
-    printfn "weak_virus : end"
+    printfn "weakVirus : end"
 
-let strong_virus_one_step () = 
-    printfn "strong_virus_one_step : begin"
+[<Test>]
+let ``strongVirusOneStep`` () = 
+    printfn "strongVirusOneStep : begin"
     let n = 100000
     let net = Network((fun () -> 5ul))
-    printfn "strong_virus_one_step : point 1"
+    printfn "strongVirusOneStep : point 1"
     for i in 1..n do
-        net.addComputer(Computer(10ul))
-    printfn "strong_virus_one_step : point 2"
+        net.AddComputer(Computer(10ul))
+    printfn "strongVirusOneStep : point 2"
     for i in 1..n-1 do
-        net.addEdge 0 i
-    printfn "strong_virus_one_step : point 3"
-    net.addVirus 0
+        net.AddEdge 0 i
+    printfn "strongVirusOneStep : point 3"
+    net.AddVirus 0
     nothingBesideZero net
-    net.step()
-    for x in net.getState() do
+    net.Step()
+    for x in net.GetState() do
         x |> should be True
-    printfn "strong_virus_one_step : end"
+    printfn "strongVirusOneStep : end"
 
-let strong_virus_tree () = 
-    printfn "strong_virus_tree : begin"
+[<Test>]
+let ``strongVirusTree`` () = 
+    printfn "strongVirusTree : begin"
     let n = (1 <<< 16) - 1
     let net = Network((fun () -> 5ul))
-    printfn "strong_virus_tree : point 1"
+    printfn "strongVirusTree : point 1"
     for i in 1..n do
-        net.addComputer(Computer(10ul))
-    printfn "strong_virus_tree : point 2"
+        net.AddComputer(Computer(10ul))
+    printfn "strongVirusTree : point 2"
     let mutable i = 0
     while ((i + 1) <<< 1) < n do // (i + 1) <<< 1 equal i * 2 + 2
-        net.addEdge i ((i <<< 1) + 1)
-        net.addEdge i ((i <<< 1) + 2)
+        net.AddEdge i ((i <<< 1) + 1)
+        net.AddEdge i ((i <<< 1) + 2)
         i <- i + 1
-    printfn "strong_virus_tree : point 3"
-    net.addVirus 0
+    printfn "strongVirusTree : point 3"
+    net.AddVirus 0
     let mutable infestedTo : uint64 = 1UL
     nothingBesideZero net
     for i in 0..62 do
-        net.step()
+        net.Step()
         infestedTo <- (infestedTo <<< 1) + 1UL
-        let x = net.getState()
+        let x = net.GetState()
         for j in 0..n-1 do
             x.[j] |> should equal (uint64 j < infestedTo)
-    printfn "strong_virus_tree : end"
+    printfn "strongVirusTree : end"
     
-let random_virus_tree ()= 
-    printfn "random_virus_tree : begin"
+[<Test>]
+let ``randomVirusTree`` ()= 
+    printfn "randomVirusTree : begin"
     let n = (1 <<< 16) - 1
     let net = Network (fun () -> uint32 <| rnd.Next())
-    printfn "random_virus_tree : point 1"
+    printfn "randomVirusTree : point 1"
     for i in 1..n do
-        net.addComputer(Computer(uint32 <| rnd.Next()))
-    printfn "random_virus_tree : point 2"
+        net.AddComputer(Computer(uint32 <| rnd.Next()))
+    printfn "randomVirusTree : point 2"
     let mutable i = 0
     while ((i + 1) <<< 1) < n do // (i + 1) <<< 1 equal i * 2 + 2
-        net.addEdge i ((i <<< 1) + 1)
-        net.addEdge i ((i <<< 1) + 2)
+        net.AddEdge i ((i <<< 1) + 1)
+        net.AddEdge i ((i <<< 1) + 2)
         i <- i + 1
-    printfn "random_virus_tree : point 3"
-    net.addVirus 0
+    printfn "randomVirusTree : point 3"
+    net.AddVirus 0
     let mutable infestedTo : uint64 = 1UL
     nothingBesideZero net
     for i in 0..62 do
-        net.step()
+        net.Step()
         if i < 63 then
             infestedTo <- (infestedTo <<< 1) + 1UL
-        let now = net.getState()
+        let now = net.GetState()
         now.[0] |> should be True
-        for j in 1..n-1 do
+        for j in 1..n - 1 do
             if now.[j] then now.[(j - 1) >>> 1] |> should be True // parent infested
             if (i < 63) && (uint64 j >= infestedTo) then now.[j] |> should be False
-    printfn "random_virus_tree : end"
+    printfn "randomVirusTree : end"
     
-[<EntryPoint>]
-let main argv =
-    weak_virus()
-    strong_virus_one_step()
-    strong_virus_tree()
-    random_virus_tree ()
-    0 // return an integer exit code
+//[<EntryPoint>]
+//let main argv =
+//    0 // return an integer exit code
--- lastSteps/lastSteps/Network.fs
+++ lastSteps/lastSteps/Network.fs
@@ -9,40 +9,67 @@ let readInt = Int32.Parse << Console.ReadLine
 let readStr = Console.ReadLine
 let rnd = System.Random()
 
+/// <summary>
+/// Simulates the behavior of the computer under virus attacks
+/// </summary>
 type Computer (aChance : uint32 ) =
     let mutable containsVirus = false
-    member v.setVirus() = containsVirus <- true;
-    member v.probe(chance) = if not containsVirus then containsVirus <- chance < aChance;
-    member v.isInfected() = containsVirus
+    member v.SetVirus() = containsVirus <- true;
+    member v.Probe(chance) = if not containsVirus then containsVirus <- chance < aChance;
+    member v.IsInfected() = containsVirus
 
+/// <summary>
+/// Emulator of a network with viruses
+/// </summary>
 type Network (gen : unit -> uint32) = 
     let mutable graph : array<list<int> > = [|[]|]
     let mutable computers : array<Computer> = [||]
     let mutable setOfInfected : list<int> = []
-    member v.addEdge first second = 
+
+/// <summary>
+/// Add an edge between the 'first' and 'second' computer
+/// </summary>
+    member v.AddEdge first second = 
         assert(0 <= first && first < graph.Length)
         assert(0 <= second && second < graph.Length)
         graph.[first] <- second::graph.[first]
         graph.[second] <- first::graph.[second]
-    member v.addVirus pos =
+
+/// <summary>
+/// Add a virus to the computer with number of 'pos'
+/// </summary>
+    member v.AddVirus pos =
         assert (0 <= pos && pos < computers.Length)
-        computers.[pos].setVirus()
+        computers.[pos].SetVirus()
         setOfInfected <- pos::setOfInfected
-    member v.addComputer (comp : Computer) =
+
+/// <summary>
+/// Add a computer to the network
+/// The smallest free number is assigned to it
+/// </summary>
+    member v.AddComputer (comp : Computer) =
         computers <- Array.append computers [|comp|]
         graph <- Array.append graph [|[]|]
-    member v.getState() = 
+
+/// <summary>
+/// Emulator of a network with viruses
+/// </summary>
+    member v.GetState() = 
         let mutable ans : array<bool> = Array.init computers.Length (fun _ -> false)
-        for i in 0..computers.Length-1 do
-            ans.[i] <- computers.[i].isInfected()
+        for i in 0..computers.Length - 1 do
+            ans.[i] <- computers.[i].IsInfected()
         ans
-    member v.step() = 
+
+/// <summary>
+/// Next iteration of network emulation
+/// </summary>
+    member v.Step() = 
         let mutable tmp = []
         for x in setOfInfected do
             for v in graph.[x] do
-                if not <| computers.[v].isInfected() then
-                    computers.[v].probe <| gen()
-                    if computers.[v].isInfected() then
+                if not <| computers.[v].IsInfected() then
+                    computers.[v].Probe <| gen()
+                    if computers.[v].IsInfected() then
                         tmp <- v::tmp
         for x in tmp do
             setOfInfected <- x::setOfInfected
